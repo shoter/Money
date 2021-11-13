8002c8173b73abd5e943cd6e5c5a079f21d3829f
Damian Laczak <shoter@users.noreply.github.com>
2021-11-13 10:25:27 +0100

    feat: all stock price service

---
+++ MoneyBack/StockPrices/AllStockPriceService.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MoneyBack.StockPrices
+{
+    public class AllStockPriceService : IAllStockPriceService
+    {
+        private Dictionary<StockPriceType, IStockPriceService> stockPriceServices = new Dictionary<StockPriceType, IStockPriceService>();
+
+        public AllStockPriceService(IStockPriceService[] stockPriceServices)
+        {
+            foreach(var ss in stockPriceServices)
+            {
+                this.stockPriceServices.Add(ss.StockPriceType, ss);
+            }
+        }
+        public decimal GetStockPrice(StockPriceType stockType, string symbol)
+        {
+            return stockPriceServices[stockType].GetStockPrice(symbol);
+        }
+    }
+}
---
+++ MoneyBack/StockPrices/IAllStockPriceService.cs
@@ -0,0 +1,7 @@
+﻿namespace MoneyBack.StockPrices
+{
+    public interface IAllStockPriceService
+    {
+        decimal GetStockPrice(StockPriceType stockType, string symbol);
+    }
+}
